7eedbf0534abc9ee17250bc41545a2695cb1f720
lightfor <death_light@foxmail.com>
2017-05-10 16:27:41 +0800

    add pom.xml and the example code

---
+++ src/main/java/Example.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Example
+ * Created by Light on 2017/3/11.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
